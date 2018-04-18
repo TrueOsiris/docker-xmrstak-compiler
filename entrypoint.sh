f02f0862510cc00b87a8a48d441feeee4535219c
Osiris <tim@chaubet.be>
2018-04-18 15:21:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-RUN cd /mnt/hostvol/ \
- && git clone https://github.com/fireice-uk/xmr-stak.git
- && cd xmr-stak \
- && mkdir build \
- && cd build
+#!/bin/bash
+cd /mnt/hostvol/ \
+git clone https://github.com/fireice-uk/xmr-stak.git
+cd xmr-stak \
+mkdir build \
+cd build
